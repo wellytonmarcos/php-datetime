68cdaa1bfba190f9a2d5bdf843614a68d74d5985
Wellyton Marcos Silva de Oliveira <wellyton.oliveira@GRUPOSN.CORP>
2020-03-25 10:28:36 -0300

    Criando Objetos DateTime

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 Modelos de Trabalho com Datas no PHP
 ### Definindo Formatos
 Definine a configuração dos formatos padrões que se pode utilizar
-´´´
+```
 define("DATETIME_BR", "d/m/Y H:i:s"); //Data e Hora no Formato Brasileiro
 define("DATE_BR", "d/m/Y"); //Exibindo apenas a data no Formato Brasileiro
-´´´´
+```
+
+### Criando objjetos DateTime
+Definine a configuração dos formatos padrões que se pode utilizar
+```
+$dateTimeNow = new DateTime();   //Criando uma nova data hora de agora
+```
--- index.php
+++ index.php
@@ -7,3 +7,12 @@
 define("DATETIME_BR", "d/m/Y H:i:s"); //Data e Hora no Formato Brasileiro
 define("DATE_BR", "d/m/Y"); //Exibindo apenas a data no Formato Brasileiro
 
+//Criando Objetos
+$dateTimeNow = new DateTime();                  //Criando uma nova data hora de agora
+//var_dump($dateTimeNow);                       //Descomentar caso queira debugar
+/* 
+Saida esperada: 
+object(DateTime)[1]
+  public 'date' => string '2020-03-25 13:10:07.215701' (length=26) (Data e Hora atual)
+  public 'timezone_type' => int 3                                   Time Zone brasileira
+  public 'timezone' => string 'UTC' (length=3)  */
