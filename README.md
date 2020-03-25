f20903e0f617c3b03f384c446c96776493966f7c
Wellyton Marcos Silva de Oliveira <wellyton.oliveira@GRUPOSN.CORP>
2020-03-25 10:54:12 -0300

    Formato das Datas

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Classe DateTime no PHP
 Modelos de Trabalho com Datas no PHP
+
 ### Definindo Formatos
 Definine a configuração dos formatos padrões que se pode utilizar
 ```
@@ -7,8 +8,22 @@ define("DATETIME_BR", "d/m/Y H:i:s"); //Data e Hora no Formato Brasileiro
 define("DATE_BR", "d/m/Y"); //Exibindo apenas a data no Formato Brasileiro
 ```
 
-### Criando objjetos DateTime
-Definine a configuração dos formatos padrões que se pode utilizar
+### Criando objetos DateTime
+Criar objetos a partir da Classe DateTime
 ```
-$dateTimeNow = new DateTime();   //Criando uma nova data hora de agora
+$agora = new DateTime();                  //Criando uma nova data hora de agora
+$avulsa = new DateTime("2020-02-02");        //Criando uma data a partir de uma data específica (Usar Americano)
+$estatica = DateTime::createFromFormat('Y-m-d', "2020-02-02"); //Criando a partir de uma objeto estático a partir de um formato, sem instaciar
 ```
+
+### Formatando Objetos
+Formata Objetos da Classe DateTime
+```
+$agoraDateTimeBr = $agora->format(DATETIME_BR); //Utilizando data e hora BR
+$agoraDateBr = $agora->format(DATE_BR); //Utilizando apenas data BR
+$somenteDia = $agora->format('d'); //Exibe apenas o dia (numero)
+$somenteDiaS = $agora->format('l'); //Exibe apenas o dia (Str da semana)
+$somenteMes = $agora->format('m'); //Exibe apenas o mes (numero)
+$somenteMesS = $agora->format('M'); //Exibe apenas o mes (Str abreviado)
+```
+*Consultar formatos em: https://www.php.net/manual/en/function.date.php *
--- index.php
+++ index.php
@@ -3,16 +3,39 @@
  * [ DateTime ] http://php.net/manual/en/class.datetime.php
  */
 
- /** DEFININDO FORMATOS DE DATAS */
+// DEFININDO FORMATOS DE DATAS 
 define("DATETIME_BR", "d/m/Y H:i:s"); //Data e Hora no Formato Brasileiro
 define("DATE_BR", "d/m/Y"); //Exibindo apenas a data no Formato Brasileiro
 
-//Criando Objetos
-$dateTimeNow = new DateTime();                  //Criando uma nova data hora de agora
-//var_dump($dateTimeNow);                       //Descomentar caso queira debugar
-/* 
-Saida esperada: 
-object(DateTime)[1]
-  public 'date' => string '2020-03-25 13:10:07.215701' (length=26) (Data e Hora atual)
-  public 'timezone_type' => int 3                                   Time Zone brasileira
-  public 'timezone' => string 'UTC' (length=3)  */
+// CRIANDO OBJETOS 
+$agora = new DateTime();                  //Criando uma nova data hora de agora
+$avulsa = new DateTime("2020-02-02");        //Criando uma data a partir de uma data específica (Usar Americano)
+$estatica = DateTime::createFromFormat('Y-m-d', "2020-02-02"); //Criando a partir de uma objeto estático a partir de um formato, sem instaciar
+
+var_dump(
+    $agora,
+    $avulsa,
+    $estatica
+);
+
+//FORMATANDO DATETIME
+$agoraDateTimeBr = $agora->format(DATETIME_BR); //Utilizando data e hora BR
+$agoraDateBr = $agora->format(DATE_BR); //Utilizando apenas data BR
+
+var_dump(
+    $agoraDateTimeBr,
+    $agoraDateBr,
+);
+
+$somenteDia = $agora->format('d'); //Exibe apenas o dia (numero)
+$somenteDiaS = $agora->format('l'); //Exibe apenas o dia (Str da semana)
+$somenteMes = $agora->format('m'); //Exibe apenas o mes (numero)
+$somenteMesS = $agora->format('M'); //Exibe apenas o mes (Str abreviado)
+//Consultar formatos em: https://www.php.net/manual/en/function.date.php
+
+var_dump([
+    $somenteDia,
+    $somenteDiaS,
+    $somenteMes,
+    $somenteMesS
+]);
